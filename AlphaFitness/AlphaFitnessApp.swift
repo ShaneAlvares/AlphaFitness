158328e0eab08fe98c3d34fa33e2adf951d0be90
Frank Alvares <shaneencoreit@gmail.com>
2023-03-10 07:34:38 +0530

    first commit

---
+++ AlphaFitness/AlphaFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphaFitnessApp.swift
+//  AlphaFitness
+//
+//  Created by Frank Alvares on 2023-03-10.
+//
+
+import SwiftUI
+
+@main
+struct AlphaFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
